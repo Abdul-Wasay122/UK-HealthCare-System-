0de334d9dbf34bcfadaa1cb2d5a01af236d3f751
Abdul Wasay <aa23adm@herts.ac.uk>
2026-01-01 12:22:06 +0000

    Update AppointmentController.java

--- controller/AppointmentController.java
+++ controller/AppointmentController.java
@@ -1,4 +1,41 @@
 package controller;
 
-public class AppointmentController {
-}
+
+import data.AppointmentRepository;
+import model.Appointment;
+
+import java.util.List;
+
+    public class AppointmentController {
+
+
+        private AppointmentRepository appointmentRepository;
+
+        public void bookAppointment(Appointment appointment) {
+            appointmentRepository.addAppointment(appointment);
+        }
+
+
+        public AppointmentController(AppointmentRepository appointmentRepository) {
+            this.appointmentRepository = appointmentRepository;
+        }
+
+
+
+        public void cancelAppointment(String appointmentId) {
+            appointmentRepository.updateAppointmentStatus(appointmentId, "Cancelled");
+        }
+
+        public List<Appointment> getAppointmentsByPatient(String patientId) {
+            return appointmentRepository.getAppointmentsByPatient(patientId);
+        }
+
+        // View all appointments
+        public List<Appointment> getAllAppointments() {
+            return appointmentRepository.loadAppointments();
+
+        }
+    }
+
+
+
