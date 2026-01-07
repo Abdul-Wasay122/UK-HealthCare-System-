a5dfc568c11a4c769c50c6bd85935285215d8920
Abdul Wasay <aa23adm@herts.ac.uk>
2026-01-07 12:16:40 +0000

    Refactor Patient class with additional attributes

--- src/model/patient.java
+++ src/model/patient.java
@@ -1,39 +1,112 @@
 package model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
-public class patient extends user {
-private Date Dob;
+
+public class Patient extends User {
+
 private String medicalHistory;
 private String insuranceNumber;
+private LocalDate dob;
+private String firstName;
+private String lastName;
+private String nhsNumber;
+private String gender;
+    private String address;
+    private String postcode;
+    private String emergencyContactName;
+    private String emergencyContactPhone;
+    private LocalDate registrationDate;
+    private String gpSurgeryId;
 
-public patient(int userId, String name, String email, String phone, String address, String password,
-               Date dob, String medicalHistory, String insuranceNumber) {
- super(userId, name, email, phone, address, password);
- this.Dob = dob;
- this.medicalHistory = medicalHistory;
- this.insuranceNumber = insuranceNumber;
+public Patient(String userId, String firstName, String lastName, String email, String phone,
+               String address, String postcode, String password, LocalDate dob, String nhsNumber,
+               String gender, String emergencyContactName, String emergencyContactPhone,
+               LocalDate registrationDate, String gpSurgeryId, String medicalHistory, String insuranceNumber) {
+ super(userId, firstName,lastName , email, phone, address, password);
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = dob;
+    this.nhsNumber = nhsNumber;
+    this.gender = gender;
+    this.address = address;
+    this.postcode = postcode;
+    this.emergencyContactName = emergencyContactName;
+    this.emergencyContactPhone = emergencyContactPhone;
+    this.registrationDate = registrationDate;
+    this.gpSurgeryId = gpSurgeryId;
+    this.medicalHistory = medicalHistory;
+    this.insuranceNumber = insuranceNumber;
+
 
 }
-    public void bookAppointment(){
-    System.out.println("Appointment Booking Requested");
-    //logic handled by appointmentController
-}
-    // cancelAppointment()
-    public void cancelAppointment() {
-        // logic handled by AppointmentController
-        System.out.println("Appointment cancellation requested.");
+
+    public LocalDate getDob() {
+    return dob;
     }
 
-    // viewPrescription()
-    public void viewPrescription() {
-        // data retrieved via PrescriptionController
-        System.out.println("Viewing prescriptions.");
+    public String getFirstName() {
+    return firstName;
+    }
+    public String getLastName() {
+    return lastName;
+    }
+    public String getNhsNumber() {
+    return nhsNumber;
+    }
+    public String getGender() {
+    return gender;
+    }
+    public String getAddress() {
+    return address;
+    }
+    public String getPostcode() {
+    return postcode;
+    }
+    public String getEmergencyContactName() {
+    return emergencyContactName;
+    }
+    public String getEmergencyContactPhone() {
+    return emergencyContactPhone;
+    }
+    public LocalDate getRegistrationDate() {
+    return registrationDate;
+
+
+}
+public String getGpSurgeryId() {
+    return gpSurgeryId;
+}
+public String getMedicalHistory() {
+    return medicalHistory;
+}
+public String getFullAddress(){
+    return address + " " + postcode;
+}
+
+    public String toCSV() {
+        return String.join(",",
+                userId,
+                firstName,
+                lastName,
+                dob.toString(),
+                nhsNumber,
+                gender,
+                phone,
+                email,
+                address,
+                postcode,
+                emergencyContactName,
+                emergencyContactPhone,
+                registrationDate.toString(),
+                gpSurgeryId
+
+        );
     }
 
-    // viewReferral()
-    public void viewReferral() {
-        // data retrieved via ReferralController
-        System.out.println("Viewing referrals.");
+    public String getInsuranceNumber() {
+    return insuranceNumber;
     }
 }
+
